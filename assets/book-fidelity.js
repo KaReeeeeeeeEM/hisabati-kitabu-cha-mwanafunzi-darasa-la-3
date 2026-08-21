efeefa60202524470d296772c7ed6b410e9f2b8f
_running_program <129178005+KaReeeeeeeeEM@users.noreply.github.com>
2026-08-21 22:42:21 +0300

    Raise page vi institution line above viewer controls

--- assets/book-fidelity.js
+++ assets/book-fidelity.js
@@ -2,7 +2,7 @@
   if (!document.querySelector('link[data-book-fidelity-styles]')) {
     const auditedStyles = document.createElement('link');
     auditedStyles.rel = 'stylesheet';
-    auditedStyles.href = './assets/fonts.css?v=20260821-fidelity-pass-24';
+    auditedStyles.href = './assets/fonts.css?v=20260821-fidelity-pass-25';
     auditedStyles.dataset.bookFidelityStyles = 'true';
     document.head.append(auditedStyles);
   }
